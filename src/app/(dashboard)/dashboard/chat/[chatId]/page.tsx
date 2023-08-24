00192c2f36add5ffa1830c41728313257841abfa
hasanify <hasanali.abps@gmail.com>
2023-08-25 01:51:11 +0530

    feat: send messages

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -4,26 +4,29 @@ import { db } from "@/lib/db";
 import { messageArrayValidator } from "@/lib/validations/message";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import Image from "next/image";
+import Messages from "@/components/Messages";
+import ChatInput from "@/components/ChatInput";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { chatId: string };
-}) {
-  const session = await getServerSession(authOptions);
-  if (!session) notFound();
-  const [userId1, userId2] = params.chatId.split("--");
-  const { user } = session;
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: { chatId: string };
+// }) {
+//   const session = await getServerSession(authOptions);
+//   if (!session) notFound();
+//   const [userId1, userId2] = params.chatId.split("--");
+//   const { user } = session;
 
-  const chatPartnerId = user.id === userId1 ? userId2 : userId1;
-  const chatPartnerRaw = (await fetchRedis(
-    "get",
-    `user:${chatPartnerId}`
-  )) as string;
-  const chatPartner = JSON.parse(chatPartnerRaw) as User;
+//   const chatPartnerId = user.id === userId1 ? userId2 : userId1;
+//   const chatPartnerRaw = (await fetchRedis(
+//     "get",
+//     `user:${chatPartnerId}`
+//   )) as string;
+//   const chatPartner = JSON.parse(chatPartnerRaw) as User;
 
-  return { title: `NextChat | ${chatPartner.name}` };
-}
+//   return { title: `NextChat | ${chatPartner.name}` };
+// }
 
 interface PageProps {
   params: {
@@ -63,10 +66,46 @@ const page = async ({ params }: PageProps) => {
 
   const chatPartnerId = user.id === userId1 ? userId2 : userId1;
 
-  const chatPartner = await db.get(`user:${chatPartnerId}`);
+  const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User;
   const initialMessages = await getChatMessages(chatId);
 
-  return <div>{chatId}</div>;
+  return (
+    <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh - 6rem)]">
+      <div className=" flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+        <div className="relative items-center flex space-x-4">
+          <div className="relative">
+            <div className="relative w-8 h-8 sm:h-12 sm:w-12">
+              <Image
+                fill
+                referrerPolicy="no-referrer"
+                src={chatPartner.image}
+                alt={`${chatPartner.name} Profile Picture`}
+                className="rounded-full"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col leading-tight">
+            <div className="flex text-xl items-center">
+              <span className="text-gray-700 mr-3 font-semibold">
+                {chatPartner.name}
+              </span>
+            </div>
+
+            <span className="text-sm text-gray-600">{chatPartner.email}</span>
+          </div>
+        </div>
+      </div>
+      <Messages
+        chatId={chatId}
+        initialMessages={initialMessages}
+        sessionId={session.user.id}
+        sessionImg={session.user.image}
+        chatPartner={chatPartner}
+      />
+      <ChatInput chatPartner={chatPartner} chatId={chatId} />
+    </div>
+  );
 };
 
 export default page;
---
+++ src/components/Messages.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Message } from "@/lib/validations/message";
+import moment from "moment";
+import Image from "next/image";
+import { FC, useRef, useState } from "react";
+
+interface MessagesProps {
+  initialMessages: Message[];
+  sessionId: string;
+  chatId: string;
+  sessionImg: string | null | undefined;
+  chatPartner: User;
+}
+
+const Messages: FC<MessagesProps> = ({
+  initialMessages,
+  sessionId,
+  chatId,
+  chatPartner,
+  sessionImg,
+}) => {
+  const [messages, setMessages] = useState<Message[]>(initialMessages);
+
+  const scrollDownRef = useRef<HTMLDivElement | null>(null);
+
+  return (
+    <div
+      id="messages"
+      className="flex h-full flex-1 flex-col-reverse gap-1 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+    >
+      <div ref={scrollDownRef} />
+
+      {messages.map((message, index) => {
+        const isCurrentUser = message.senderId === sessionId;
+
+        const hasNextMessageFromSameUser =
+          messages[index - 1]?.senderId === messages[index].senderId;
+
+        return (
+          <div
+            className="chat-message"
+            key={`${message.id}-${message.timestamp}`}
+          >
+            <div
+              className={cn("flex items-end", {
+                "justify-end": isCurrentUser,
+              })}
+            >
+              <div
+                className={cn(
+                  "flex flex-col space-y-2 text-base max-w-xs mx-2",
+                  {
+                    "order-1 items-end": isCurrentUser,
+                    "order-2 items-start": !isCurrentUser,
+                  }
+                )}
+              >
+                <span
+                  className={cn("px-4 py-2 rounded-lg inline-block", {
+                    "bg-indigo-600 text-white": isCurrentUser,
+                    "bg-gray-200 text-gray-900": !isCurrentUser,
+                    "rounded-br-none":
+                      !hasNextMessageFromSameUser && isCurrentUser,
+                    "rounded-bl-none":
+                      !hasNextMessageFromSameUser && !isCurrentUser,
+                  })}
+                >
+                  {message.text}{" "}
+                  <span className="ml-2 text-xs text-gray-400">
+                    {moment(message.timestamp).format("Do MMM YYYY, hh:mm a")}
+                  </span>
+                </span>
+              </div>
+
+              <div
+                className={cn("relative w-6 h-6", {
+                  "order-2": isCurrentUser,
+                  "order-1": !isCurrentUser,
+                  invisible: hasNextMessageFromSameUser,
+                })}
+              >
+                <Image
+                  fill
+                  src={
+                    isCurrentUser ? (sessionImg as string) : chatPartner.image
+                  }
+                  alt="Profile picture"
+                  referrerPolicy="no-referrer"
+                  className="rounded-full"
+                />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Messages;
