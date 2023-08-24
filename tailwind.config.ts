eb0ab7b3b39840945813fb841f5e049ed21eb7d7
hasanify <hasanali.abps@gmail.com>
2023-08-24 14:29:45 +0530

    setup app colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,16 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#F9F9F9",
+        light: "#EBCBAE",
+        dark: "#8F8787",
+        accent: "#A6E4E7",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
