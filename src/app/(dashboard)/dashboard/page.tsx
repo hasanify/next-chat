e280efea840ec30b42b7f5f96671e4bd82d4fa85
hasanify <hasanali.abps@gmail.com>
2023-08-24 23:03:38 +0530

    remove placeholder session details from dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,14 +1,7 @@
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
 import React from "react";
 
 const Dashboard = async () => {
-  const session = await getServerSession(authOptions);
-  return (
-    <div>
-      <pre>{JSON.stringify(session)} </pre>
-    </div>
-  );
+  return <div>Dashboard</div>;
 };
 
 export default Dashboard;
